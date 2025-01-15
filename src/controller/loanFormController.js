6c72618f60db54af16c39c35a5dcabf753cfc514
Osbeam <osbeamit1@gmail.com>
2025-01-15 13:11:42 +0530

    channel partner done

--- src/controller/loanFormController.js
+++ src/controller/loanFormController.js
@@ -17,39 +17,6 @@ const uploadimg = imgUpload.fields([
 
 
 
-// LoanFormController.post("/createLoanForm", uploadimg, async (req, res) => {
-//   try {
-//     console.log('Request Body:', req.body);
-//     console.log('Uploaded Files:', req.files);
-
-//     // Extract file paths from req.files and add them to the loanData object
-//     const loanData = { 
-//       ...req.body,
-//       Photo: req.files?.Photo ? req.files.Photo[0].path : null,
-//       PanCard: req.files?.PanCard ? req.files.PanCard[0].path : null,
-//       AadharCard: req.files?.AadharCard ? req.files.AadharCard[0].path : null,
-//       BankStatement: req.files?.BankStatement ? req.files.BankStatement[0].path : null,
-//       SalarySlip: req.files?.SalarySlip ? req.files.SalarySlip[0].path : null,
-//       TwoYearITR: req.files?.TwoYearITR ? req.files.TwoYearITR[0].path : null
-//     };
-
-//     const loanCreated = await LoanForm.create(loanData);
-
-//     sendResponse(res, 200, "Success", {
-//       success: true,
-//       message: "Loan Form created successfully!",
-//       loanData: loanCreated,
-//     });
-//   } catch (error) {
-//     console.log(error);
-//     sendResponse(res, 500, "Failed", {
-//       message: error.message || "Internal server error",
-//     });
-//   }
-// });
-
-
-
 
 LoanFormController.put("/createOrUpdateLoanForm/:id?", uploadimg, async (req, res) => {
   try {
